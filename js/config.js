db25bb4e6031b0e74e0dfb3a1b4adec4b97278a1
littleban0305 <littleban0305@gmail.com>
2026-08-11 11:51:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
+const CONFIG = {
+    APP_NAME: "ELOCase",
+    VERSION: "0.1.0",
+    CURRENCY_NAME: "ELOCoin",
 
+    API_URL: "https://script.google.com/macros/s/AKfycbzfCdLdBUEv2tXaguqS0Fm9uf9PslygEYpL16eYrKVqEdoX9Z6AMpfU0Wd7xIUmI7FfDg/exec"
+};
