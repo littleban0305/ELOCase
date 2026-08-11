c02e5dc05e71ead5d3c269549bac4e5c216c196e
littleban0305 <littleban0305@gmail.com>
2026-08-11 11:53:49 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,9 @@
+function updateBalance(eloCoin) {
+    const balanceElement = document.querySelector(".balance-value");
 
+    if (!balanceElement) {
+        return;
+    }
+
+    balanceElement.textContent = Number(eloCoin).toLocaleString();
+}
