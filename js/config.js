d72b9d3bf7700cec2d0aa1def969f6ae748a13f9
littleban0305 <littleban0305@gmail.com>
2026-08-17 10:59:44 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,5 +3,8 @@ const CONFIG = {
     VERSION: "0.1.0",
     CURRENCY_NAME: "ELOCoin",
 
-    API_URL: "https://script.google.com/macros/s/AKfycbzfCdLdBUEv2tXaguqS0Fm9uf9PslygEYpL16eYrKVqEdoX9Z6AMpfU0Wd7xIUmI7FfDg/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbzfCdLdBUEv2tXaguqS0Fm9uf9PslygEYpL16eYrKVqEdoX9Z6AMpfU0Wd7xIUmI7FfDg/exec",
+
+    GOOGLE_CLIENT_ID:
+        "725217385610-676f29r52ou8khmfdkst5nuohofdblg6.apps.googleusercontent.com"
 };
