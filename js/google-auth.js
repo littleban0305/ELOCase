8f4088891b63a5faf64ba4ff693ae9c2a32fd3d9
littleban0305 <littleban0305@gmail.com>
2026-08-17 11:02:12 +0800

    Update google-auth.js

--- js/google-auth.js
+++ js/google-auth.js
@@ -56,10 +56,68 @@ document.addEventListener(
 async function handleGoogleLogin(response){
 
 
+    const googleToken =
+        response.credential;
+
+
     console.log(
         "Google Token:",
-        response.credential
+        googleToken
     );
 
+
+    try {
+
+
+        const result =
+            await googleLogin(
+                googleToken
+            );
+
+
+        console.log(
+            "Google 登入成功:",
+            result
+        );
+
+
+        /*
+         * 儲存 Session
+         */
+
+        localStorage.setItem(
+            "sessionToken",
+            result.sessionToken
+        );
+
+
+        localStorage.setItem(
+            "elocaseUser",
+            JSON.stringify(
+                result.user
+            )
+        );
+
+
+        location.href =
+            "index.html";
+
+
+
+    }catch(error){
+
+
+        console.error(
+            "Google 登入失敗:",
+            error
+        );
+
+
+        alert(
+            error.message
+        );
+
+
+    }
 
 }
